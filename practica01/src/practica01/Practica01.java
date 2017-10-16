bef2235416f8eed7cb2d5b3ddb82ebcd1f12c524
mpuenteb <mpuenteb@gmail.com>
2017-10-16 18:02:52 +0200

    Upload practica 01

---
+++ practica01/src/practica01/Practica01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica01;
+
+/**
+ *
+ * @author dam2
+ */
+public class Practica01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       fraVentana ventana=new fraVentana();
+       ventana.setVisible(true);
+//       ventana.setTitle("Centro "+ventana.getSize().width+" X "+ventana.getSize().height);
+       
+    }
+    
+}
